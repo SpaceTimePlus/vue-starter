81266827e9510b1e1922ac949da7bbb14996c5c8
liuhui <liuhui@weiwuu.net>
2018-06-11 12:08:05 +0800

    添加远程返回数据缓存

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,4 +1,5 @@
 import axios from './fetch'
+import localforage from 'localforage'
 
 const debug = process.env.NODE_ENV !== 'production'
 
@@ -97,4 +98,58 @@ export function del (url, responseType, headers) {
     responseType,
     headers
   })
+}
+
+
+// 缓存操作 =============================================================================================================
+
+/***
+ * 设置本地缓存过期时间
+ * @param key
+ * @param min
+ */
+function setExpiration(key, min) {
+  let expirationSet = new Date().getTime() + 1000 * 60 * min
+  localforage.setItem(key + '_expiration', expirationSet)
+}
+
+/***
+ * 判断本地缓存是否有效
+ * @param key
+ */
+async function keyEnable(key) {
+  let expirationGet = await localforage.getItem((key + '_expiration')
+  let enable = expirationGet > new Date().getTime()
+  return enable
+}
+
+
+/***
+ * 获取远程数据（缓存版）
+ * @param key
+ * @returns {Promise<*>}
+ */
+async function getReturn(url, responseType, headers,  key, min) {
+
+  let location = await localforage.getItem(key)
+
+  if (location && keyEnable(key)) {
+
+    return location
+
+  } else {
+
+    let result = await get(url, responseType, headers)
+
+    if (result) {
+
+      localforage.setItem(key, result)
+
+      setExpiration(key, min)
+
+      return result
+
+    }
+
+  }
 }
